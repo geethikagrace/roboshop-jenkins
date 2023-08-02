0821c60c1a94c489be07e47fe7184b6558f4d087
geethika <geethilka2219@gmail.com>
2023-08-02 11:55:33 +0530

    sonar scanner

--- vars/mavan.groovy
+++ vars/mavan.groovy
@@ -15,27 +15,27 @@ def call() {
 
 
         stages{
-            stage (code compile){
+            stage ('code compile'){
                 steps{
                     sh 'code compile'
                 }
             }
-            stage (code quality){
+            stage ('code quality'){
                 steps{
                     sh 'sonar-scanner '
                 }
             }
-            stage (Unit Test Cases){
+            stage ('Unit Test Cases'){
                 steps{
                     sh 'Unit Test Cases'
                 }
             }
-            stage (Chechmarx SAST scan){
+            stage ('Chechmarx SAST scan'){
                 steps{
                     sh 'Chechmarx SAST scan'
                 }
             }
-            stage (Chechmarx SCA scan){
+            stage ('Chechmarx SCA scan'){
                 steps{
                     sh 'Chechmarx SCA scan'
                 }
--- vars/python.groovy
+++ vars/python.groovy
@@ -15,22 +15,22 @@ def call() {
 
 
         stages{
-            stage (code quality){
+            stage ('code quality'){
                 steps{
-                    sh 'sonar-scanner -Dsonar.projectkey=${component}'
+                    sh 'sonar-scanner -Dsonar.projectkey=${component} -Dsonar.host.url = http://172.31.82.179:9000 -Dsonar.login=admin -Dsonar.password=admin123'
                 }
             }
-            stage (Unit Test Cases){
+            stage ('Unit Test Cases'){
                 steps{
                     sh 'Unit Test Cases'
                 }
             }
-            stage (Chechmarx SAST scan){
+            stage ('Chechmarx SAST scan'){
                 steps{
                     sh 'Chechmarx SAST scan'
                 }
             }
-            stage (Chechmarx SCA scan){
+            stage ('Chechmarx SCA scan'){
                 steps{
                     sh 'Chechmarx SCA scan'
                 }
