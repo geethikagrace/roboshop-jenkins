ce01982ed4f63765d497ce836c0c8adffc14cdf0
geethika <geethilka2219@gmail.com>
2023-08-02 13:30:25 +0530

    sonar scanner

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -15,25 +15,31 @@ def call() {
 
         stages {
 
-            stage('Code Quality') {
-                steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.179:9000 -Dsonar.login=admin -Dsonar.password=admin123 '
+//            stage('Code Quality') {
+//                steps {
+//                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.179:9000 -Dsonar.login=admin -Dsonar.password=admin123 '
 //                    sh 'echo Code Quality'
-                }
-            }
-            stage('Unit Test Cases') {
-                steps {
-                    sh 'echo Unit Test Cases'
-                }
-            }
-            stage('Chechmarx SAST scan') {
-                steps {
-                    sh 'echo Chechmarx SAST scan'
-                }
-            }
-            stage('Chechmarx SCA scan') {
-                steps {
-                    sh 'echo  Chechmarx SCA scan'
+//                }
+//            }
+//            stage('Unit Test Cases') {
+//                steps {
+//                    sh 'echo Unit Test Cases'
+//                }
+//            }
+//            stage('Chechmarx SAST scan') {
+//                steps {
+//                    sh 'echo Chechmarx SAST scan'
+//                }
+//            }
+//            stage('Chechmarx SCA scan') {
+//                steps {
+//                    sh 'echo  Chechmarx SCA scan'
+//                }
+//            }
+
+            stage('one'){
+                steps{
+                    sh 'echo hello world '
                 }
             }
         }
