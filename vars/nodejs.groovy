34c0b02aea1e13fafab3cb3547a9d64048777bcb
geethika <geethilka2219@gmail.com>
2023-08-02 17:41:44 +0530

    user

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -17,8 +17,8 @@ def call() {
 
             stage('Code Quality') {
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.179:9000  '
-//                    sh 'echo Code Quality'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.82.179:9000 Dsonar.login=admin -Dsonar.password=admin123 '
+
                 }
             }
             stage('Unit Test Cases') {
